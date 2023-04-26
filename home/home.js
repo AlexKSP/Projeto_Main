874fc190034b70d1888fc12f1ffbbe8ca0d1bf95
AlexKSP <127268092+AlexKSP@users.noreply.github.com>
2023-04-26 17:53:06 -0300

    0.4
    mudanças

--- home/home.js
+++ home/home.js
@@ -8,8 +8,43 @@ function registrar(){
 /* Criar objeto que seja um posto qual a pessoa possa inserir e excluir.*/
 
 function Comentario () {
-    var postagem = document.getElementById("texto")
-    var postar = new String(postagem)
-    alert(`${postar}`)
+    let postagem = document.getElementById('texto')
+    let postar = String(postagem.value)
+    if (postar != "" && postar.length >= 10) {
+        //alert('certo')
+        // cria um novo elemento div
+        // e dá à ele conteúdo
+        let newDiv = document.createElement("div")
+        let newMsg = document.createTextNode(`${postar}`)
+        newDiv.appendChild(newMsg) //adiciona o nó de texto à nova div criada
+
+        // adiciona o novo elemento criado e seu conteúdo ao DOM
+        var divAtual = document.getElementById("div1") 
+        document.body.insertBefore(newDiv, divAtual)
+        document.getElementById('resp').innerHTML = 'tchau'
+        
+        
+    } else {
+        alert('Campo vazio ou com menos de 10 caracteres')
+    }
+    
+    
 }
 
+/*  -declaração de um objeto:
+    var automovel = {
+        cor: azul,
+        marca: fiat,
+        ano: 2022
+        modelo: argo
+    } 
+    -propriedade de um obejeto é tudo o que está dentro de {}
+    -caso eu queira adicionar uma nova propriedade:
+    carro.categoria = carro;
+
+    -declaração de array:
+    var matriz = [4 , 2]
+    -adicionando um numero novo:
+    matriz[2] = 5;
+    
+*/
